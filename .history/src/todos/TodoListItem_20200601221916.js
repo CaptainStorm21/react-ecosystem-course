0bf3f4bde8bf0f0bc6c582e0e7eabea626be96e1
captainstorm21 <tikana4@yahoo.com>
2020-06-01 22:20:41 -0700

    Create TodoListItem_20200601221916.js

---
+++ .history/src/todos/TodoListItem_20200601221916.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./TodoListItem.css";
+
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
+  <div className="todo-item-container">
+    <ol className="todo-align">{todo.text}</ol>
+    <div className="button-container">
+      {todo.isCompleted ? null : (
+        <button
+          onClick={() => onCompletedPressed(todo.text)}
+          className="completed-button"
+        >
+          Mark As Completed
+        </button>
+      )}
+
+      <button
+        onClick={() => onRemovePressed(todo.text)}
+        className="remove-button"
+      >
+        Remove
+      </button>
+    </div>
+  </div>
+);
+
+export default TodoListItem;
